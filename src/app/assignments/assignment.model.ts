b15b73fd81cb03452e2c5a8fe5e476c64a9a46aa
Sirianne-EKRA <dykoffi2000@gmail.com>
2024-06-01 21:15:53 +0000

    Finish work

--- src/app/assignments/assignment.model.ts
+++ src/app/assignments/assignment.model.ts
@@ -1,8 +1,11 @@
+import { Matiere } from "../matieres/matiere.model";
+
 export class Assignment {
     _id!: string;
     nom!: string;
     nom_eleve!: string;
     matiere!: string;
+    matiereDetails!: Matiere;
     dateDeRendu!: Date;
     rendu!: boolean;
     note!: number;
